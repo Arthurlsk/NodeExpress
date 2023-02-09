417bac327f63d8a4a8692755819da1906cc94273
ArthurLsk <arthurleonardo.strobel@gmail.com>
2023-02-09 10:29:51 -0300

    WIP

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,20 @@
+class ContactController {
+  index() {
+    //listar todos registros
+  }
+  show(){
+    //obter um registro
+  }
+  store(){
+    //criar novo registro
+  }
+  update(){
+    //editar registro
+  }
+  delete(){
+    //deletar registro
+  }
+
+}
+//Singleton
+module.exports = new ContactController();
